cae18b8c39053f16987a6635f78a7c8098207c13
JustinACoder <justin.tovich@gmail.com>
2025-05-29 13:13:04 -0400

    Updated config + installed necessary packages

--- frontend/orval.config.js
+++ frontend/orval.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from "orval";
-
-export default defineConfig({
+module.exports = {
   api: {
     output: {
-      mode: "tags-split",
+      mode: "tags",
       target: "src/lib/api",
       schemas: "src/lib/models",
       client: "react-query",
       mock: true,
+      baseUrl: "http://localhost:3000",
     },
     input: {
       target: "./openapi.json",
     },
   },
-});
+};
